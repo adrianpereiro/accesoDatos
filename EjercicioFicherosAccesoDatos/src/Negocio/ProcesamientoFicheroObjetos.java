1a706404c11fa2dcaacd0ba8ac6a3df9bac4fe74
FP <FP@DESKTOP-5LVH4IU>
2020-10-21 10:06:48 +0200

    .

--- EjercicioFicherosAccesoDatos/src/Negocio/ProcesamientoFicheroObjetos.java
+++ EjercicioFicherosAccesoDatos/src/Negocio/ProcesamientoFicheroObjetos.java
@@ -15,62 +15,65 @@ import Modelo.Libro;
 import Modelo.Personaje;
 
 public class ProcesamientoFicheroObjetos extends ProcesamientoFichero {
-	
+
 	@Override
 	public ArrayList<Libro> leerFichero() {
 		File f = new File("");
 		ArrayList<Libro> listaLibros = new ArrayList<>();
-		try {
-			@SuppressWarnings("resource")
-			ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(f));
-			for (;;) {
-				Libro l = (Libro) entrada.readObject();
-				listaLibros.add(l);
+		if (ProcesamientoFichero.existeFichero(f) == false) {
+			try {
+				@SuppressWarnings("resource")
+				ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(f));
+				for (;;) {
+					Libro l = (Libro) entrada.readObject();
+					listaLibros.add(l);
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (EOFException e) {
+				System.out.println("Llego al final del fichero");
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (EOFException e) {
-			System.out.println("Llego al final del fichero");
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
 		}
 		return listaLibros;
+
 	}
-		
 
 	@Override
 	public void guardarFichero(ArrayList<Libro> listaLibros) {
 		File f = new File("");
-		try {
-			ObjectOutputStream ficheroSalida = new ObjectOutputStream(new FileOutputStream(f));
-			for (Libro libro : listaLibros) {
-				String tituloLibro = libro.getTituloLibro();
-				String Editorial = libro.getEditorial();
-				String Autor = libro.getAutor();
-				LocalDate fechaPublicacion = libro.getFechaPublicacion();
-				String cadenaFechaPublicacion = fechaPublicacion.toString();
-				String Genero = libro.getGenero();
-				String libroGuardar = tituloLibro + ";" + Editorial + ";" + Autor + ";" + cadenaFechaPublicacion + ";"
-						+ Genero + ";";
-				ArrayList<Personaje> listaPersonajes = libro.getPersonajesPrincipales();
-				for (Personaje personajes : listaPersonajes) {
-					String nombre = personajes.getNombre();
-					String importancia = personajes.getImportancia();
-					libroGuardar = libroGuardar + "-" + nombre + "," + importancia;
+		if (ProcesamientoFichero.existeFichero(f) == false) {
+			try {
+				ObjectOutputStream ficheroSalida = new ObjectOutputStream(new FileOutputStream(f));
+				for (Libro libro : listaLibros) {
+					String tituloLibro = libro.getTituloLibro();
+					String Editorial = libro.getEditorial();
+					String Autor = libro.getAutor();
+					LocalDate fechaPublicacion = libro.getFechaPublicacion();
+					String cadenaFechaPublicacion = fechaPublicacion.toString();
+					String Genero = libro.getGenero();
+					String libroGuardar = tituloLibro + ";" + Editorial + ";" + Autor + ";" + cadenaFechaPublicacion
+							+ ";" + Genero + ";";
+					ArrayList<Personaje> listaPersonajes = libro.getPersonajesPrincipales();
+					for (Personaje personajes : listaPersonajes) {
+						String nombre = personajes.getNombre();
+						String importancia = personajes.getImportancia();
+						libroGuardar = libroGuardar + "-" + nombre + "," + importancia;
+					}
+					ficheroSalida.writeObject(libroGuardar + "\n");
 				}
-				ficheroSalida.writeObject(libroGuardar+"\n");
+
+				ficheroSalida.close();
+
+			} catch (FileNotFoundException fnfe) {
+				System.out.println("Error: El fichero no existe. ");
+			} catch (IOException ioe) {
+				System.out.println("Error: Fallo en la escritura en el fichero. ");
 			}
 
-			ficheroSalida.close();
-
-		} catch (FileNotFoundException fnfe) {
-			System.out.println("Error: El fichero no existe. ");
-		} catch (IOException ioe) {
-			System.out.println("Error: Fallo en la escritura en el fichero. ");
 		}
-		
 	}
-
 }
--- EjercicioFicherosAccesoDatos/src/Negocio/ProcesamientoFicheroPlano.java
+++ EjercicioFicherosAccesoDatos/src/Negocio/ProcesamientoFicheroPlano.java
@@ -17,9 +17,9 @@ public class ProcesamientoFicheroPlano extends ProcesamientoFichero {
 	@Override
 	public ArrayList<Libro> leerFichero() {
 		ArrayList<Libro> listaLibros = new ArrayList<>();
-		
-			File f = new File("");
-			if (ProcesamientoFichero.existeFichero(f) == false) {
+
+		File f = new File("");
+		if (ProcesamientoFichero.existeFichero(f) == false) {
 			ArrayList<Personaje> listaPersonajes = new ArrayList<>();
 			FileInputStream fis;
 			try {
@@ -58,41 +58,40 @@ public class ProcesamientoFicheroPlano extends ProcesamientoFichero {
 
 	public void guardarFichero(ArrayList<Libro> listaLibros) {
 		File f = new File("C:\\Users\\PC33\\Desktop\\prueba.txt");
-		FileWriter fiw = null;
-		try {
-			fiw = new FileWriter(f);
-
-			for (Libro libro : listaLibros) {
-				String tituloLibro = libro.getTituloLibro();
-				String Editorial = libro.getEditorial();
-				String Autor = libro.getAutor();
-				LocalDate fechaPublicacion = libro.getFechaPublicacion();
-				String cadenaFechaPublicacion = fechaPublicacion.toString();
-				String Genero = libro.getGenero();
-				String libroGuardar = tituloLibro + ";" + Editorial + ";" + Autor + ";" + cadenaFechaPublicacion + ";"
-						+ Genero + ";";
-				ArrayList<Personaje> listaPersonajes = libro.getPersonajesPrincipales();
-				for (Personaje personajes : listaPersonajes) {
-					String nombre = personajes.getNombre();
-					String importancia = personajes.getImportancia();
-					libroGuardar = libroGuardar + "-" + nombre + "," + importancia;
-				}
-				fiw.write(libroGuardar + "\n");
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
+		if (ProcesamientoFichero.existeFichero(f) == false) {
+			FileWriter fiw = null;
 			try {
-				fiw.close();
+				fiw = new FileWriter(f);
+
+				for (Libro libro : listaLibros) {
+					String tituloLibro = libro.getTituloLibro();
+					String Editorial = libro.getEditorial();
+					String Autor = libro.getAutor();
+					LocalDate fechaPublicacion = libro.getFechaPublicacion();
+					String cadenaFechaPublicacion = fechaPublicacion.toString();
+					String Genero = libro.getGenero();
+					String libroGuardar = tituloLibro + ";" + Editorial + ";" + Autor + ";" + cadenaFechaPublicacion + ";" + Genero + ";";
+					ArrayList<Personaje> listaPersonajes = libro.getPersonajesPrincipales();
+					for (Personaje personajes : listaPersonajes) {
+						String nombre = personajes.getNombre();
+						String importancia = personajes.getImportancia();
+						libroGuardar = libroGuardar + "-" + nombre + "," + importancia;
+					}
+					fiw.write(libroGuardar + "\n");
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
+			} finally {
+				try {
+					fiw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
-
-
 
 	public static Boolean borrarFichero() {
 		return null;
