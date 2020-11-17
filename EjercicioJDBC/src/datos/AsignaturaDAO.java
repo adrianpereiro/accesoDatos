7724ad4e81c80ed2ebf0aec26be8d3cae72a4a05
adrianpereiro <adrianpereiro99@gmail.com>
2020-11-17 09:27:33 +0100

    .

--- EjercicioJDBC/src/datos/AsignaturaDAO.java
+++ EjercicioJDBC/src/datos/AsignaturaDAO.java
@@ -1,8 +1,43 @@
 package datos;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
-public class AsignaturaDAO {
+import modelo.IAsignaturaDAO;
+import modelo.Entidades.Asignatura;
+
+public class AsignaturaDAO implements IAsignaturaDAO{
 	ConexionMySQL conexion = new ConexionMySQL();
 	Connection con = conexion.creacionConexion();
+	
+	@Override
+	public void insertar(Asignatura asignatura) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void insertarMultiple(ArrayList<Asignatura> listaAsignaturas) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void eliminar(Asignatura asignatura) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void eliminarMultiple(ArrayList<Asignatura> listaAsignaturas) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void modificar(Asignatura asignatura) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void modificarMultiple(ArrayList<Asignatura> listaAsignaturas) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- EjercicioJDBC/src/datos/CicloDAO.java
+++ EjercicioJDBC/src/datos/CicloDAO.java
@@ -1,8 +1,43 @@
 package datos;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
-public class CicloDAO {
+import modelo.ICicloDAO;
+import modelo.Entidades.Ciclo;
+
+public class CicloDAO implements ICicloDAO {
 	ConexionMySQL conexion = new ConexionMySQL();
 	Connection con = conexion.creacionConexion();
+	
+	@Override
+	public void insertar(Ciclo ciclo) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void insertarMultiple(ArrayList<Ciclo> listaCiclos) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void eliminar(Ciclo ciclo) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void eliminarMultiple(ArrayList<Ciclo> listaCiclos) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void modificar(Ciclo ciclo) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void modificarMultiple(ArrayList<Ciclo> listaCiclos) {
+		// TODO Auto-generated method stub
+		
+	}
 }
