fd790b795138a92b8606c55641d4f90678d24dda
adrianpereiro <adrianpereiro99@gmail.com>
2020-11-25 09:44:19 +0100

    .

--- EjercicioJDBC/src/modelo/Entidades/AsignaturaVOMapping.java
+++ EjercicioJDBC/src/modelo/Entidades/AsignaturaVOMapping.java
@@ -1,5 +1,21 @@
 package modelo.Entidades;
 
-public class AsignaturaVOMapping {
+import java.util.ArrayList;
 
+import vista.Presentacion.AsignaturaVO;
+
+public class AsignaturaVOMapping {
+	public AsignaturaVO crearAsignaturaVO(Asignatura asignatura) {
+		AsignaturaVO asignaturaVO = new AsignaturaVO(asignatura.getNombre(), asignatura.getHorasSemanales(), asignatura.getIdCiclo());
+		return asignaturaVO;
+	}
+	
+	public ArrayList<AsignaturaVO> crearListaAsignaturaVO(ArrayList<Asignatura> listaAsignaturas) {
+		ArrayList<AsignaturaVO> listaAsignaturasVO = new ArrayList<>();
+		for(Asignatura asignatura : listaAsignaturas) {
+			AsignaturaVO asignaturaVO = new AsignaturaVO(asignatura.getNombre(), asignatura.getHorasSemanales(), asignatura.getIdCiclo());
+			listaAsignaturasVO.add(asignaturaVO);
+		}
+		return listaAsignaturasVO;
+	}
 }
