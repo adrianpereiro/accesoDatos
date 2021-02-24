7a632cf0702573aeb08be3887bbf5341c378f5ba
adrianpereiro <adrianpereiro99@gmail.com>
2021-02-24 10:48:06 +0100

    .

--- EjercicioORM/src/main/java/vista/vo/Ejercicio.java
+++ EjercicioORM/src/main/java/vista/vo/Ejercicio.java
@@ -26,103 +26,101 @@ import modelo.entidades.Usuario;
 public class Ejercicio {
 
 	public static void main(String[] args) {
-/*		Ejercicio 7.1
- 		Libro l = new Libro("1", "ola que tal", "folja", 20);
-		LibroDao lDao = new LibroDao();
-		lDao.insertar(l);
-		Autor a = new Autor(0, "Juan", "Gadid", null);
-		AutorDao aDao = new AutorDao();
-		aDao.insertar(a);
+//		Ejercicio 7.1
+//		Libro l = new Libro("1", "ola que tal", "folja", 20);
+//		LibroDao lDao = new LibroDao();
+//		lDao.insertar(l);
+//		Autor a = new Autor(0, "Juan", "Gadid", null);
+//		AutorDao aDao = new AutorDao();
+//		aDao.insertar(a);
+//
+//		a.getLibros().add(l);
+//
+//		aDao.modificar(a);
 		
-		a.getLibros().add(l);
-		
-		aDao.modificar(a);
-*/		
-		
-/*	Ejercicio 7.2
-		Libro l = new Libro("1", "ola que tal", "folja", 20);
-		LibroDao lDao = new LibroDao();
-		
-		EjemplarDao eDao = new EjemplarDao();
-		Ejemplar e = new Ejemplar(1, l, 2, "ASFASFD");
-		
-		eDao.insertar(e);
-		
-		lDao.eliminar("1");
-*/
-		
-/*		Ejercicio 7.3 a
-		Date current = new java.sql.Date(new java.util.Date().getTime());
-		Usuario u = new Usuario("Adrian", "Pereiro Miguel", current, "54131324L");
-		Contactousuario cu = new Contactousuario(u, "a@g.com");
-		UsuarioDao uDao = new UsuarioDao();
-		uDao.insertar(u);
-		ContactousuarioDao cuDao = new ContactousuarioDao();
-		cuDao.insertar(cu);
-*/
 
-/* 		Ejercicio 7.3 b
-		UsuarioDao uDao = new UsuarioDao();
-		uDao.eliminar(12);
-*/
-		
-/*		Ejercicio 7.4 
-		Date current = new java.sql.Date(new java.util.Date().getTime());
-		Usuario u = new Usuario("Adrian", "Pereiro Miguel", current);
-		UsuarioDao uDao = new UsuarioDao();
-		
-		Contactousuario cu = new Contactousuario(u, "a@g.com");
-		ContactousuarioDao cuDao = new ContactousuarioDao();
+//		Ejercicio 7.2
+//		Libro l = new Libro("1", "ola que tal", "folja", 20);
+//		LibroDao lDao = new LibroDao();
+//
+//		EjemplarDao eDao = new EjemplarDao();
+//		Ejemplar e = new Ejemplar(1, l, 2, "ASFASFD");
+//
+//		eDao.insertar(e);
+//
+//		lDao.eliminar("1");
 
-		Libro l = new Libro("10", "Un titulo", "Una editoral", (float)20);
-		LibroDao lDao = new LibroDao();
-		
-		Ejemplar e = new Ejemplar(1, l, 1, "estado");
-		EjemplarDao eDao = new EjemplarDao();
-		
-		Date datePrestamo = new java.sql.Date(2021, 1, 10);
-		Prestamo p =  new Prestamo(e, u, datePrestamo, current);
-		PrestamoDao pDao = new PrestamoDao();
-		
-		uDao.insertar(u);
-		cuDao.insertar(cu);
-		lDao.insertar(l);
-		eDao.insertar(e);
-		pDao.insertar(p);
-		
-		Date current = new java.sql.Date(new java.util.Date().getTime());
-		PrestamoDao pDao = new PrestamoDao();
-		UsuarioDao uDao = new UsuarioDao();
-		Usuario u = uDao.buscar(11);
-		Set<Prestamo> listaPrestamos =  u.getPrestamos();
-		for(Prestamo p : listaPrestamos) {
-			p.setFechaPrestamo(current);
-			pDao.modificar(p);
-		}
-*/		
-		UsuarioDao uDao = new UsuarioDao();
-/*		Ejercicio 8.1
-		uDao.buscarDNI();
-*/
+//		Ejercicio 7.3 a
+//		Date current = new java.sql.Date(new java.util.Date().getTime());
+//		Usuario u = new Usuario("Adrian", "Pereiro Miguel", current, "54131324L");
+//		Contactousuario cu = new Contactousuario(u, "a@g.com");
+//		UsuarioDao uDao = new UsuarioDao();
+//		uDao.insertar(u);
+//		ContactousuarioDao cuDao = new ContactousuarioDao();
+//		cuDao.insertar(cu);
+//
+//		Ejercicio 7.3 b
+//		UsuarioDao uDao = new UsuarioDao();
+//		uDao.eliminar(12);
 
-/*		Ejercicio 8.2
-		uDao.usuarios_con_prestamos();
-*/
-/*		Ejercicio 8.3		
-		uDao.menoresEdad();
-*/	
-		
+//		Ejercicio 7.4
+//		Date current = new java.sql.Date(new java.util.Date().getTime());
+//		Usuario u = new Usuario("Adrian", "Pereiro Miguel", current, "54131324L");
+//		UsuarioDao uDao = new UsuarioDao();
+//
+//		Contactousuario cu = new Contactousuario(u, "a@g.com");
+//		ContactousuarioDao cuDao = new ContactousuarioDao();
+//
+//		Libro l = new Libro("10", "Un titulo", "Una editoral", (float) 20);
+//		LibroDao lDao = new LibroDao();
+//
+//		Ejemplar e = new Ejemplar(1, l, 1, "estado");
+//		EjemplarDao eDao = new EjemplarDao();
+//
+//		Date datePrestamo = new java.sql.Date(2021, 1, 10);
+//		Prestamo p = new Prestamo(e, u, datePrestamo, current);
+//		PrestamoDao pDao = new PrestamoDao();
+//
+//		uDao.insertar(u);
+//		cuDao.insertar(cu);
+//		lDao.insertar(l);
+//		eDao.insertar(e);
+//		pDao.insertar(p);
+//
+//		Date current = new java.sql.Date(new java.util.Date().getTime());
+//		PrestamoDao pDao = new PrestamoDao();
+//		UsuarioDao uDao = new UsuarioDao();
+//		Usuario u = uDao.buscar(11);
+//		Set<Prestamo> listaPrestamos = u.getPrestamos();
+//		for (Prestamo p : listaPrestamos) {
+//			if (p.getFechaDevolucion() == null) {
+//				p.setFechaPrestamo(current);
+//				Dao.modificar(p);
+//			}
+//		}
+
+		UsuarioDao uDao = new UsuarioDao();
+
+//		Ejercicio 8.1
+//		uDao.buscarDNI();
+
+//		Ejercicio 8.2
+//		uDao.usuarios_con_prestamos();
+
+//		Ejercicio 8.3
+//		uDao.menoresEdad();
+
 		LibroDao lDao = new LibroDao();
-		
-/*		Ejercicio 8.4
-		lDao.librosPrestados();
-*/
-/*		Ejercicio 8.5
-		 lDao.autorLibro();
-*/
-/*		Ejercicio 8.6
-		lDao.precioLibro();
-*/
+
+//		Ejercicio 8.4
+//		lDao.librosPrestados();
+
+//		Ejercicio 8.5
+//		lDao.autorLibro();
+
+//		Ejercicio 8.6
+//		lDao.precioLibro();
+
 	}
 
 }
